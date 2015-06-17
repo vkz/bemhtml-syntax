5b2e03f1492a858085ed5f36c6a3aa1b240cb7cc
vkz <vladilen.kozin@gmail.com>
2015-06-17 13:11:48 +0300

    K-api and K-tests

--- test/basic/info5.bemhtml.js
+++ test/basic/info5.bemhtml.js
@@ -12,6 +12,7 @@
 block('b-bla')(
   tag()('span'),
   mod('0-mode', 'v2').tag()('a'),
+  //  mod 0-mode v2, tag:'a'
   mix()([{ elemMods: { m2: 'v2' } }]),
   js()(true)
 )
--- test/common.js
+++ test/common.js
@@ -7,9 +7,11 @@ var syntax = require(".."),
         'utf8'),
     compat = require("bemhtml-compat"),
     esprima = require("esprima"),
-    esgen = require("escodegen").generate,
+    es = require("escodegen"),
+    esgen = es.generate,
     path = require("path"),
-    fs = require("fs");
+    fs = require("fs"),
+    beautify = require("js-beautify").js_beautify;
 
 var pp = require("zeHelpers").prettyPrint;
 
@@ -75,6 +77,42 @@ function testCompile(source, json, html, target) {
 }
 common.testCompile = testCompile;
 
+function testKCompile(source, json, html, target) {
+  var code = getSource(source),
+      compiled = syntax.kcompile(code),
+      result = html || (json && toHtml(compat.transpile(code), json));
+
+  var poptions = {range: true, tokens: true, comment: true};
+
+  // Test against a hand-written template when supplied
+  if (target) {
+    target = getSource(target);
+    // generated code
+    var cast = esprima.parse(compiled, poptions);
+    var tast = esprima.parse(target, poptions);
+    cast = es.attachComments(cast, cast.comments, cast.tokens);
+    tast = es.attachComments(tast, tast.comments, tast.tokens);
+    // check esprima+escodegen equality
+    assert.equal(
+      es.generate(cast, {comment: true}),
+      es.generate(tast, {comment: true}));
+    // check js-beautify equality with newlines preserved
+    assert.equal(
+      beautify(compiled, {"preserve_newlines": true}),
+      beautify(target, {"preserve_newlines": true}));
+    // HTML via target
+    json && assert.equal(
+      toHtml(compiled, json),
+      toHtml(target, json));
+  };
+
+  // HTML provided or via compat
+  result && assert.equal(
+    toHtml(compiled, json),
+    result);
+}
+common.testKCompile = testKCompile;
+
 function maybeRead(f) {
   var utf8 = { encoding:'utf8' };
   try {
@@ -85,7 +123,11 @@ function maybeRead(f) {
 }
 common.maybeRead = maybeRead;
 
-function testDir(dirname) {
+function testDir(dirname, test) {
+  // Parameterise with test-arg:
+  // test === testCompile to check BEMHTML grammars
+  // test === testKCompile to check Krasota grammars
+  test = (typeof test === 'function' && test) || testCompile;
   var dir = path.join(path.dirname(module.filename), dirname),
       tests = fs.readdirSync(dir).filter(function (f) {
         return /\.bemhtml$/i.test(f);
@@ -99,8 +141,7 @@ function testDir(dirname) {
             html = maybeRead(base + '.html'),
             msg = maybeRead(base + '.msg') || name;
         it(msg, function () {
-          testCompile(source, json, html, target);
-          // testKParse(source);
+          test(source, json, html, target);
         });
       });
 }
